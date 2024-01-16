61f8ce622f15f42e8a9dadde69fd21d623e2f0aa
Hüseyin ULUĞ <huseyin.ulug@dcscustoms.com.tr>
2024-01-16 08:14:33 +0300

    Added new cases in Create Master Data

--- cypress/fixtures/createMasterDataLocators.js
+++ cypress/fixtures/createMasterDataLocators.js
@@ -2,7 +2,6 @@ export default{
     createMasterDataDashboard:"//button[@data-test-id='create-work-order-home']",
     createMasterDataModal:"//button[@id='create-master-data']",
     gtipTespitiTR:"(//input[@class='ant-checkbox-input'])[1]",
-    dcsltdModal:"//div[contains(text(),'DIGITAL CUSTOMS SERVICE LTD.')]",
     cancelCustomsFirmModal:"//span[@class='anticon anticon-close']",
     sendButtonCMD:"//button[@data-test-id='send-button']",
     saveButtonCMD:"//button[@data-test-id='save-button']",
@@ -22,6 +21,17 @@ export default{
     englishExcelTemplateCMD:"//a[contains(text(),'english-excel-template.xlsx')]",
     atezUrlButton:"//span[contains(text(),'www.atez.com')]",
     urlTrashIconCMD:"//span[@aria-label='delete']",
-    generalDocumentUploadFile:"(//input[@type='file'])[2]"
+    generalDocumentUploadFile:"(//input[@type='file'])[2]",
+    documentTrashIconCMD:"//button[@title='Remove file']",
+    downloadUploadDocumentCMD:"//button[@title='Download file']",
+    productIdTextBoxCMD:"//input[@placeholder='Ürün Kodu']",
+    businessDivisionCMD:"//span[contains(text(),'İş Bölümü')]",
+    productGroupCMD:"//span[contains(text(),'Ürün Grupları')]",
+    productDescriptionTextBoxCMD:"(//input[@data-test-id='productDescription'])[1]",
+    trashIconForProductCMD:"//button[@data-test-id='delete-action 0']",
+    deleteButtonForTrashProductCMD:"//button[@id='ok-button']",
+    atezBusinessDivisionCMD:"//div[contains(text(),'AtezDivision')]",
+    merhabaProductGroup:"(//div[contains(text(),'ELEKTRONIK')])[1]"
+
 
 };
